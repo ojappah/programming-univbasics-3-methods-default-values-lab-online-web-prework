96ad284800c9cd8e856bdeb82169a360a2dd5cdf
generous-vector-9581 <ojappah@gmail.com>
2019-08-21 01:15:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
-# Your code here
+def meal_choice("broccoli","macaroni", protein="meat")
+puts "Whether you're omnivorous, vegetarian, or vegan, you're going to be eating a nutritious meal!"
+end
+puts "What a nutritious meal!"
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
+
+def meal_choice(broc,egg)
